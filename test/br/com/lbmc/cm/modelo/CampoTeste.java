43e03c360bb00026d8732ad62e469a62a5417a7a
Luciano Bezerra <lbmcosta@yahoo.com>
2020-04-26 18:20:26 -0300

    Classe Campo

---
+++ test/br/com/lbmc/cm/modelo/CampoTeste.java
@@ -0,0 +1,48 @@
+package br.com.lbmc.cm.modelo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.*;
+
+import br.com.lbmc.cm.excecao.ExplosaoException;
+
+
+public class CampoTeste {
+	
+	private Campo campo;
+	
+	@BeforeEach
+	void iniciarCampo() {
+		campo = new Campo(3,3);
+	}
+	
+	@Test
+	void testeVizinhoLateral() {
+		Campo vizinho = new Campo(3,2);
+		boolean resultado = campo.adicionarVizinho(vizinho);
+		assertTrue(resultado);
+	}
+	
+	@Test
+	void testeVizinhoDiagonal() {
+		Campo vizinho = new Campo(2,2);
+		boolean resultado = campo.adicionarVizinho(vizinho);
+		assertTrue(resultado);
+	}
+	
+	@Test
+	void testeNaoVizinho() {
+		Campo vizinho = new Campo(3,5);
+		boolean resultado = campo.adicionarVizinho(vizinho);
+		assertFalse(resultado);
+	}
+	
+	@Test
+	void testeAbrirMinadoNaoMarcado() {
+		campo.minar();
+		assertThrows(ExplosaoException.class, () -> {
+			campo.abrir();
+			});
+	}
+
+}
